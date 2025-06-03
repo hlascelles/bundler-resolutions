8bf37af26260a1256e7b5eefef8151d966f85db4
Harry Lascelles <harry@harryl.com>
2025-06-03 21:04:08 +0100

    Working except for concrete dependencies

--- lib/bundler/resolutions/config.rb
+++ lib/bundler/resolutions/config.rb
@@ -25,8 +25,8 @@ module Bundler
           env_file = ENV["BUNDLER_RESOLUTIONS_CONFIG"]
           return env_file if env_file
 
-          # Otherwise find it in the file tree
-          dir = Dir.pwd
+          # Otherwise find it above where `BUNDLE_GEMFILE` is located, or pwd if not set
+          dir = ENV["BUNDLE_GEMFILE"] ? File.dirname(ENV["BUNDLE_GEMFILE"]) : Dir.pwd
           until File.exist?(File.join(dir, CONFIG_FILE_NAME))
             dir = File.dirname(dir)
             raise "Could not find #{CONFIG_FILE_NAME}" if dir == "/"
